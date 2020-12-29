951cbe26e5d2d01216231649985ad3ccdcfd074a
phamphibao <43768195+phamphibao@users.noreply.github.com>
2020-12-29 14:23:05 +0700

    edit slide

--- js/main.js
+++ js/main.js
@@ -1,41 +1,49 @@
 $(document).ready(function () {
+    slide_home('.main-slide-products','#pre-products','#next-products');
+    slide_home('.main-slide-latest','.pre-latest','.next-latest');
+   
 
-    $('.main-slide-products').slick({
-        // infinite: true,
-        speed: 1000,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        centerMode: true,
-        centerPadding: '10%',
-        prevArrow: ".previous",
-        nextArrow: ".next",
-        responsive: [{
-                breakpoint: 991.8,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    centerMode: true,
+
+
+    function slide_home(name_slide,previous,next) {
+        $(name_slide).slick({
+            // infinite: true,
+            speed: 1000,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: '10%',
+            prevArrow: previous,
+            nextArrow: next,
+            responsive: [{
+                    breakpoint: 991.8,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        centerMode: true,
+                    }
+                },
+                {
+                    breakpoint: 567,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        centerPadding: '11%',
+                    }
                 }
-            },
-            {
-                breakpoint: 567,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    centerPadding: '11%',
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-    });
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+    }
+
 
 });
